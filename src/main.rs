21ebebdb121ecbd49a178c4413a30463ca94fc54
Jake Ledoux <contactjakeledoux@gmail.com>
2021-08-02 13:51:15 -0400

    Basic CLI

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,91 @@
-#![allow(unused)]
-
+#[allow(unused)]
 mod scrobble;
 
+use clap::{AppSettings, Clap};
+use dialoguer::{Confirm, Input, Password};
 use scrobble::*;
 
+#[derive(Clap)]
+#[clap(version = "0.1", author = "Jake Ledoux <contactjakeledoux@gmail.com>")]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Opts {
+    file: String,
+    #[clap(long, short)]
+    dry_run: bool,
+    #[clap(long, short)]
+    skip_timezone_correction: bool,
+}
+
+fn show_scrobbles(scrobbles: &Vec<Scrobble>) {
+    for scrobble in scrobbles {
+        println!(
+            "{} - {} - {} at {}",
+            scrobble.artist,
+            scrobble.title,
+            scrobble.album,
+            scrobble.datetime.to_string()
+        );
+    }
+}
+
 fn main() {
-    let (scrobbles, errors) = parse_log(String::from(".scrobbler.log"));
-    let session = Session::new();
-    println!(
-        "{} scrobbles loaded. {} failed to load.",
-        scrobbles.len(),
-        errors
-    );
-    session.scrobble_bulk(scrobbles);
+    let opts = Opts::parse();
+
+    let mut client = Client::new();
+    let (scrobbles, errors) = parse_log(opts.file, !opts.skip_timezone_correction);
+
+    println!("{} scrobbles loaded, {} failed.", scrobbles.len(), errors);
+
+    if !opts.dry_run {
+        let username: String = match Input::new().with_prompt("Last.fm Username").interact_text() {
+            Ok(username) => username,
+            Err(_) => {
+                eprintln!("There was a problem getting the username.");
+                return;
+            }
+        };
+        let password: String = match Password::new().with_prompt("Last.fm Password").interact() {
+            Ok(password) => password,
+            Err(_) => {
+                eprintln!("There was a problem getting the password.");
+                return;
+            }
+        };
+
+        match client.authenticate(username, password) {
+            Ok(_) => println!("Authentication successful."),
+            Err(_) => {
+                eprintln!("Failed to authenticate. Verify you typed your credentials properly.");
+                return;
+            }
+        }
+    }
+
+    show_scrobbles(&scrobbles);
+
+    if !opts.dry_run {
+        match Confirm::new()
+            .with_prompt("Would you like to scrobble these tracks?")
+            .interact()
+        {
+            Ok(true) => {
+                match client.scrobble(scrobbles) {
+                    Ok((accepted, rejected)) => {
+                        println!("{} tracks scrobbled.", accepted);
+                        if rejected.len() > 0 {
+                            println!("{} tracks failed to scrobble:", rejected.len());
+                            show_scrobbles(&rejected);
+                        }
+                    }
+                    Err(_) => {
+                        eprintln!("Failed to submit scrobbles.");
+                        return;
+                    }
+                };
+            }
+            _ => {
+                println!("Aborted. Nothing has been scrobbled.")
+            }
+        }
+    }
 }
--- src/scrobble.rs
+++ src/scrobble.rs
@@ -1,55 +1,161 @@
+use chrono::{offset::LocalResult, Local, NaiveDateTime, TimeZone, Utc};
+use md5;
+use reqwest;
+use serde_json;
 use std::error::Error;
 use std::fmt;
 use std::fs;
-use std::time::Instant;
 
 #[derive(Debug)]
 pub struct ScrobbleError;
 
 impl fmt::Display for ScrobbleError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Generic ScrobbleError")
+        write!(f, "Generic scrobble error")
     }
 }
 
 impl Error for ScrobbleError {}
 
+#[derive(Debug, Clone, PartialEq)]
 pub struct Scrobble {
-    artist: String,
-    title: String,
-    album: String,
-    number: i32,
-    duration: i32,
-    time: i32, // Instant?
-    skipped: bool,
+    pub artist: String,
+    pub title: String,
+    pub album: String,
+    pub number: i32,
+    pub duration: i32,
+    pub timestamp: i64,
+    pub datetime: NaiveDateTime,
+    pub skipped: bool,
 }
 
-pub struct Session {
+pub struct Client {
+    http_client: reqwest::blocking::Client,
     endpoint: String,
+    api_key: String,
+    api_secret: String,
+    session_key: Option<String>,
 }
 
-impl Session {
-    pub fn new() -> Session {
-        Session {
-            endpoint: String::from("http://ws.audioscrobbler.com/2.0/"),
+impl Client {
+    pub fn new() -> Client {
+        Client {
+            http_client: reqwest::blocking::Client::new(),
+            endpoint: String::from("http://ws.audioscrobbler.com/2.0"),
+            api_key: String::from("9d3f8ae2c7b7e56d648780a3abf41dc6"),
+            api_secret: String::from("d65eae6191a3951aa1f9e50ac8b55ae0"),
+            session_key: None,
         }
     }
 
-    pub fn authenticate(&mut self, username: String, password: String) {}
+    fn get_signature(&self, query: &Vec<(String, String)>) -> String {
+        let mut query = query.clone();
+        query.sort_by_key(|e| e.0.clone());
 
-    pub fn scrobble(&self, scrobble: Scrobble) -> Result<(), ScrobbleError> {
-        dbg!(scrobble.artist);
+        let mut signature = String::new();
+        for (key, value) in query {
+            signature.push_str(&(key + &value));
+        }
+        signature.push_str(&self.api_secret);
+
+        let digest = md5::compute(signature);
+        let signature = format!("{:x}", digest);
+        signature
+    }
+
+    fn build_query(&self, method: &str, mut query: Vec<(String, String)>) -> Vec<(String, String)> {
+        query.push((String::from("method"), String::from(method)));
+        query.push((String::from("api_key"), self.api_key.clone()));
+        query.push((String::from("api_sig"), self.get_signature(&query)));
+        query.push((String::from("format"), String::from("json")));
+        query
+    }
+
+    fn post(
+        &self,
+        method: &str,
+        mut query: Vec<(String, String)>,
+    ) -> Result<reqwest::blocking::Response, reqwest::Error> {
+        query = self.build_query(method, query);
+        let req = self
+            .http_client
+            .post(format!("{}/", self.endpoint))
+            .form(&query);
+        let req = req.build()?;
+        self.http_client.execute(req)
+    }
+
+    pub fn authenticate(
+        &mut self,
+        username: String,
+        password: String,
+    ) -> Result<(), Box<dyn Error>> {
+        let resp = self.post(
+            "auth.getMobileSession",
+            vec![
+                (String::from("username"), username),
+                (String::from("password"), password),
+            ],
+        )?;
+        let auth_response: serde_json::Value = resp.json()?;
+
+        self.session_key = match auth_response["session"]["key"].as_str() {
+            Some(s) => Some(String::from(s)),
+            None => None,
+        };
+
+        if self.session_key.is_none() {
+            return Err(Box::new(ScrobbleError));
+        }
+
         Ok(())
     }
-    pub fn scrobble_bulk(&self, scrobbles: Vec<Scrobble>) -> Result<(), ScrobbleError> {
-        for scrobble in scrobbles {
-            self.scrobble(scrobble)?;
+
+    pub fn scrobble(
+        &self,
+        scrobbles: Vec<Scrobble>,
+    ) -> Result<(i32, Vec<Scrobble>), Box<dyn Error>> {
+        match &self.session_key {
+            Some(session_key) => {
+                let mut accepted = 0_i32;
+                let mut rejected: Vec<Scrobble> = Vec::new();
+
+                for chunk in scrobbles.chunks(50) {
+                    let mut query: Vec<(String, String)> = Vec::new();
+                    for (i, scrobble) in chunk.iter().filter(|s| !s.skipped).enumerate() {
+                        query.push((format!("artist[{}]", i), scrobble.artist.clone()));
+                        query.push((format!("track[{}]", i), scrobble.title.clone()));
+                        query.push((format!("timestamp[{}]", i), scrobble.timestamp.to_string()));
+                        query.push((format!("album[{}]", i), scrobble.album.clone()));
+                        query.push((format!("trackNumber[{}]", i), scrobble.number.to_string()));
+                    }
+                    query.push((String::from("sk"), session_key.clone()));
+
+                    let resp = self.post("track.scrobble", query)?;
+                    let status: serde_json::Value = resp.json()?;
+
+                    accepted += status["scrobbles"]["@attr"]["accepted"].as_i64().unwrap() as i32;
+
+                    // Collect rejected scrobbles to be returned
+                    for (i, scrobble) in status["scrobbles"]["scrobble"]
+                        .as_array()
+                        .unwrap()
+                        .iter()
+                        .enumerate()
+                    {
+                        if scrobble["ignoredMessage"]["code"] != "0" {
+                            rejected.push(chunk[i].clone());
+                        }
+                    }
+                }
+                Ok((accepted, rejected))
+            }
+            None => Err(Box::new(ScrobbleError)),
         }
-        Ok(())
     }
 }
 
-pub fn parse_log(filename: String) -> (Vec<Scrobble>, i32) {
+pub fn parse_log(filename: String, convert_timezone: bool) -> (Vec<Scrobble>, i32) {
     let contents = fs::read_to_string(filename).expect("failed to read file");
 
     let mut scrobbles: Vec<Scrobble> = vec![];
@@ -61,7 +167,7 @@ pub fn parse_log(filename: String) -> (Vec<Scrobble>, i32) {
             continue;
         }
 
-        let mut columns: Vec<&str> = line.split('\t').collect();
+        let columns: Vec<&str> = line.split('\t').collect();
 
         // Insufficient column data
         if columns.len() < 7 {
@@ -70,8 +176,8 @@ pub fn parse_log(filename: String) -> (Vec<Scrobble>, i32) {
         }
 
         let artist = String::from(columns[0]);
-        let title = String::from(columns[1]);
-        let album = String::from(columns[2]);
+        let album = String::from(columns[1]);
+        let title = String::from(columns[2]);
         let number: i32 = match columns[3].parse() {
             Ok(n) => n,
             Err(_) => {
@@ -94,8 +200,28 @@ pub fn parse_log(filename: String) -> (Vec<Scrobble>, i32) {
                 continue;
             }
         };
-        let time: i32 = match columns[6].parse() {
-            Ok(n) => n,
+        let (datetime, timestamp) = match columns[6].parse() {
+            Ok(n) => {
+                let dt = match Utc.timestamp_opt(n, 0) {
+                    LocalResult::Single(ts) => ts,
+                    _ => {
+                        errors += 1;
+                        continue;
+                    }
+                };
+                if convert_timezone {
+                    let dt = match Local.from_local_datetime(&dt.naive_utc()) {
+                        LocalResult::Single(ts) => ts,
+                        _ => {
+                            errors += 1;
+                            continue;
+                        }
+                    };
+                    (dt.naive_local(), dt.timestamp())
+                } else {
+                    (dt.naive_local(), n)
+                }
+            }
             Err(_) => {
                 errors += 1;
                 continue;
@@ -109,7 +235,8 @@ pub fn parse_log(filename: String) -> (Vec<Scrobble>, i32) {
             number,
             duration,
             skipped,
-            time,
+            timestamp,
+            datetime,
         };
         scrobbles.push(scrobble);
     }
